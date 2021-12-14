fcc291a488c66908fb6516753cae70d9ca23b2fd
bhvoetberg <b.h.voetberg@gmail.com>
2021-12-14 10:56:12 +0100

    React project opgetuigd npm installatie: axios & react-router-dom (5.3.0!!, geen 6.x.y) Boilerplate code verwijderd index.css voorzien van eerste specs

---
+++ src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function HomePage() {
+    return (
+        // <div className="home-page-container">
+        //     <p>Login page</p>
+        // </div>
+        <>
+            <h1>Hallo</h1>
+        </>
+    );
+}
+
+export default HomePage;
---
+++ src/pages/LoginPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LoginPage() {
+    return (
+        <div className="login-page-container">
+            <p>Login page</p>
+        </div>
+    );
+}
+
+export default LoginPage;
